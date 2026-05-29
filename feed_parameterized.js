9a204da943f38494b91f080b98476420ef6969ec
AnneLin <annelovedance0513@gmail.com>
2026-05-29 09:53:00 -0400

    add alt text to image

--- feed_parameterized.js
+++ feed_parameterized.js
@@ -248,7 +248,7 @@ $(function () { // Document ready function
   function buildEvent(obj, count) {
     let html = '<div class="event' + params.wide + '" style="flex:0 0 ' + (100 / state.elementPerRow) + '%">';
     let image_url = (obj[fields.image_url]) ? obj[fields.image_url] : "https://events.umich.edu/images/default190@2x.png";
-    let image = '<a class = "image-link" href =' + obj[fields.page_link] + '><div class = "event-image' + params.wide + '" style="background-image: url(' + image_url + ')"></div></a>';
+    let image = '<a class = "image-link" href =' + obj[fields.page_link] + '" aria-label="' + obj[fields.event_title] + '">' + '<div class = "event-image' + params.wide + '" style="background-image: url(' + image_url + ')"></div></a>';
     let title = obj[fields.title];
     let date = obj[fields.date];
     let links = obj[fields.links];
